6173dc184e09f2030004fce3eab3db547bcc9f62
vlcoscar8 <vlcoscar8@github.com>
2023-08-05 09:53:52 +0200

    Fx: toggle theme button

--- src/core/components/Switch/Switch.tsx
+++ src/core/components/Switch/Switch.tsx
@@ -5,7 +5,7 @@ import { ThemeContext } from "../../context/theme.context.tsx";
 function Switch() {
     const {isDark, toggleTheme} = useContext(ThemeContext)
 
-    return <div className={`${styles.switch} ${isDark ? styles.switchDark : styles.switchLight}`} onClick={() => toggleTheme}>
+    return <div className={`${styles.switch} ${isDark ? styles.switchDark : styles.switchLight}`} onClick={() => toggleTheme(isDark)}>
         <span className={`${isDark ? styles.dark : styles.light}`}></span>
     </div>
 }   
