ceca0c06ebbfbc0a295838be8d9e2367440d353c
vlcoscar8 <vlcoscar8@github.com>
2023-08-17 16:05:59 +0200

    feat: add ATRA project and new Blog post

--- src/core/screens/Blog/utils/PostInfo.ts
+++ src/core/screens/Blog/utils/PostInfo.ts
@@ -1,24 +1,33 @@
 export interface PostInfo {
-    title: string,
-    date: string,
-    time: string,
-    description: string
-    href?: string
+  title: string;
+  date: string;
+  time: string;
+  description: string;
+  href?: string;
 }
 
 export const PostInfo: PostInfo[] = [
-    {
-        title: 'Frontend state management with clean architecture',
-        date: '03 August 2023',
-        time: '20 min read',
-        description: 'Manage a global frontend state using clean architecture and without any external libraries step-by-step',
-        href: 'https://dev.to/oscarprdev/frontend-state-management-with-clean-architecture-22d8'
-    },
-    {
-        title: 'Intersection Observer API & Lazy loading in REACT',
-        date: '05 August 2023',
-        time: '10 min read',
-        description: 'How to create an intersection observer wrapper component in react to manage the components lazy loading',
-        href: "https://dev.to/oscarprdev/intersection-observer-api-lazy-loading-in-react-44"
-    }
-]
+  {
+    title: "Typescript + Vue linter config step by step",
+    date: "17 August 2023",
+    time: "10 min read",
+    description: "How to configure Eslint with Typescript and Vue application",
+    href: "https://dev.to/oscarprdev/typescript-vue-linter-config-step-by-step-ni8",
+  },
+  {
+    title: "Frontend state management with clean architecture",
+    date: "03 August 2023",
+    time: "20 min read",
+    description:
+      "Manage a global frontend state using clean architecture and without any external libraries step-by-step",
+    href: "https://dev.to/oscarprdev/frontend-state-management-with-clean-architecture-22d8",
+  },
+  {
+    title: "Intersection Observer API & Lazy loading in REACT",
+    date: "05 August 2023",
+    time: "10 min read",
+    description:
+      "How to create an intersection observer wrapper component in react to manage the components lazy loading",
+    href: "https://dev.to/oscarprdev/intersection-observer-api-lazy-loading-in-react-44",
+  },
+];
--- src/core/screens/Projects/utils/projects-data.ts
+++ src/core/screens/Projects/utils/projects-data.ts
@@ -1,127 +1,129 @@
-import {AngularIcon, CssIcon, JavascriptIcon, ReactIcon, SvelteIcon, TailwindIcon, TypescriptIcon} from "../../../svg";
-import chatImage from '../../../../assets/images/chat.png'
-import foroImage from '../../../../assets/images/foro.png'
-import readinglistImage from '../../../../assets/images/readinglist.png'
-import ecommerceImage from '../../../../assets/images/ecommerce.png'
-
+import {
+  CssIcon,
+  JavascriptIcon,
+  ReactIcon,
+  SvelteIcon,
+  TailwindIcon,
+  TypescriptIcon,
+} from "../../../svg";
+import chatImage from "../../../../assets/images/chat.png";
+import foroImage from "../../../../assets/images/foro.png";
+import readinglistImage from "../../../../assets/images/readinglist.png";
+import atraImage from "../../../../assets/images/atra.png";
+import VueIcon from "../../../svg/VueIcon.tsx";
 
 export interface Project {
-    id: number,
-    title: string,
-    date: string,
-    image: string,
-    description: string,
-    links: {
-      code: string,
-      app: string
-    },
-    icons:
-        {
-            svg: JSX.Element,
-            isModified: boolean
-        }[]
-
+  id: number;
+  title: string;
+  date: string;
+  image: string;
+  description: string;
+  links: {
+    code: string;
+    app: string;
+  };
+  icons: {
+    svg: JSX.Element;
+    isModified: boolean;
+  }[];
 }
 
 export const ProjectsData: Project[] = [
-    {
-        id: 1,
-        title: 'Reading List',
-        date: '20 July 2023',
-        image: readinglistImage,
-        description: 'Reading list app where the user can add books to a list and set as favourite or as already read',
-        links: {
-            code: 'https://github.com/oscarprdev/midudev-pruebas-tecnicas/tree/oscarprdev/reading-list/pruebas/01-reading-list/oscarprdev',
-            app: 'https://books-oscarprdev.netlify.app/'
-        },
-        icons: [
-            {
-                svg: TypescriptIcon(),
-                isModified: false
-            },
-            {
-                svg: SvelteIcon(),
-                isModified: true
-            },
-            {
-                svg: TailwindIcon(),
-                isModified: true
-            }
-        ]
+  {
+    id: 1,
+    title: "ATRA arquitectura",
+    date: "17 August 2023",
+    image: atraImage,
+    description: "Architecture portfolio for ATRA client",
+    links: {
+      code: "https://github.com/oscarprdev/atra-architecture",
+      app: "https://atra-architecture.netlify.app/#/",
     },
-    {
-        id: 2,
-        title: 'Forum',
-        date: '23 June 2022',
-        image: foroImage,
-        description: 'A forum app where users can create and comment topics',
-        links: {
-            code: 'https://github.com/oscarprdev/REACT--Foro_social',
-            app: 'https://react-foro.netlify.app'
-        },
-        icons: [
-            {
-                svg: JavascriptIcon(),
-                isModified: false
-            },
-            {
-                svg: ReactIcon(),
-                isModified: false
-            },
-            {
-                svg: CssIcon(),
-                isModified: true
-            },
-        ]
+    icons: [
+      {
+        svg: TypescriptIcon(),
+        isModified: false,
+      },
+      {
+        svg: VueIcon(),
+        isModified: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Reading List",
+    date: "20 July 2023",
+    image: readinglistImage,
+    description:
+      "App where the user can add books to a list and manage the book state",
+    links: {
+      code: "https://github.com/oscarprdev/midudev-pruebas-tecnicas/tree/oscarprdev/reading-list/pruebas/01-reading-list/oscarprdev",
+      app: "https://books-oscarprdev.netlify.app/",
     },
-    {
-        id: 3,
-        title: 'Chat',
-        date: '11 July 2022',
-        image: chatImage,
-        description: 'A basic chat app done with sockets.io',
-        links: {
-            code: 'https://github.com/oscarprdev/REACT-Chat-Socket',
-            app: 'https://react-chat-socket-oscar.netlify.app/'
-        },
-        icons: [
-            {
-                svg: JavascriptIcon(),
-                isModified: false
-            },
-            {
-                svg: ReactIcon(),
-                isModified: false
-            },
-            {
-                svg: CssIcon(),
-                isModified: true
-            },
-        ]
+    icons: [
+      {
+        svg: TypescriptIcon(),
+        isModified: false,
+      },
+      {
+        svg: SvelteIcon(),
+        isModified: true,
+      },
+      {
+        svg: TailwindIcon(),
+        isModified: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Forum",
+    date: "23 June 2022",
+    image: foroImage,
+    description: "A forum app where users can create and comment topics",
+    links: {
+      code: "https://github.com/oscarprdev/REACT--Foro_social",
+      app: "https://react-foro.netlify.app",
     },
-    {
-        id: 4,
-        title: 'E-commerce',
-        date: '28 May 2022',
-        image: ecommerceImage,
-        description: 'A sport shoes e-commerce with shopping cart and product detail screen',
-        links: {
-            code: 'https://github.com/oscarprdev/Angular--Sneakers-ecommerce',
-            app: 'https://sneakers-ecommerce-nine.vercel.app/'
-        },
-        icons: [
-            {
-                svg: TypescriptIcon(),
-                isModified: false
-            },
-            {
-                svg: AngularIcon(),
-                isModified: false
-            },
-            {
-                svg: CssIcon(),
-                isModified: true
-            },
-        ]
+    icons: [
+      {
+        svg: JavascriptIcon(),
+        isModified: false,
+      },
+      {
+        svg: ReactIcon(),
+        isModified: false,
+      },
+      {
+        svg: CssIcon(),
+        isModified: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Chat",
+    date: "11 July 2022",
+    image: chatImage,
+    description: "A basic chat app done with sockets.io",
+    links: {
+      code: "https://github.com/oscarprdev/REACT-Chat-Socket",
+      app: "https://react-chat-socket-oscar.netlify.app/",
     },
-]
+    icons: [
+      {
+        svg: JavascriptIcon(),
+        isModified: false,
+      },
+      {
+        svg: ReactIcon(),
+        isModified: false,
+      },
+      {
+        svg: CssIcon(),
+        isModified: true,
+      },
+    ],
+  },
+];
