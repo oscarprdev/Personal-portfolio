ff3fed1d521f4f45e41ec4c4b854e6de13065189
vlcoscar8 <vlcoscar8@github.com>
2023-08-19 10:38:21 +0200

    fix: Floating project images

--- src/core/components/LazyComponentWrapper/LazyComponentWrapper.tsx
+++ src/core/components/LazyComponentWrapper/LazyComponentWrapper.tsx
@@ -1,53 +1,59 @@
-import {ReactNode, useContext, useEffect, useRef, useState} from "react";
-import styles from './Lazy.module.css'
-import {LocationContext} from "../../context/location.context.tsx";
+import { ReactNode, useContext, useEffect, useRef, useState } from "react";
+import styles from "./Lazy.module.css";
+import { LocationContext } from "../../context/location.context.tsx";
 
 interface LazyComponentWrapperProps {
-    children: ReactNode;
-    className: string;
-    id: string;
+  children: ReactNode;
+  className: string;
+  id: string;
 }
 
-function LazyComponentWrapper({ children, className, id }: LazyComponentWrapperProps) {
-    const {updateLocation} = useContext(LocationContext)
+function LazyComponentWrapper({
+  children,
+  className,
+  id,
+}: LazyComponentWrapperProps) {
+  const { updateLocation } = useContext(LocationContext);
 
-    const [isVisible, setIsVisible] = useState(false);
-    const componentRef = useRef<HTMLElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const componentRef = useRef<HTMLElement | null>(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        setIsVisible(true);
-                        updateLocation(entry.target.id)
-                        observer.unobserve(entry.target);
-                    }
-
-                });
-            },
-            { threshold: 0.5 } as IntersectionObserverInit
-        );
-
-        if (componentRef.current) {
-            observer.observe(componentRef.current!);
-        }
-
-        return () => {
-            if (componentRef.current) {
-                observer.unobserve(componentRef.current!);
-            }
-        };
-    }, [updateLocation]);
-
-    return (
-        <section
-            ref={componentRef}
-            className={`${className} ${isVisible ? styles.visible : styles.notVisible}`}
-            id={id}>
-            {isVisible && children}
-        </section>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            updateLocation(entry.target.id);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 } as IntersectionObserverInit,
     );
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current!);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current!);
+      }
+    };
+  }, [updateLocation]);
+
+  return (
+    <section
+      ref={componentRef}
+      className={`${className} ${
+        isVisible ? styles.visible : styles.notVisible
+      }`}
+      id={id}
+    >
+      {isVisible && children}
+    </section>
+  );
 }
 
 export default LazyComponentWrapper;
